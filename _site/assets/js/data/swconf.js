ede7198faa7abc79625e5f1b591c912857444201
qrxnz <send@qrxnz.dev>
2024-09-23 20:29:11 +0200

    custom avatar

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1726776738',resources: [
+    cacheName: 'chirpy-1727116098',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
