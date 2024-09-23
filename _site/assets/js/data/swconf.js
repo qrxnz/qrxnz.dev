3f9a0ce20f843885828f509e5d99f9a6106cf911
qrxnz <send@qrxnz.dev>
2024-09-23 20:37:01 +0200

    username

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1727116098',resources: [
+    cacheName: 'chirpy-1727116600',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
