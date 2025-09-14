b7f69ee0c278844f272772266c4ad63648280c31
Karol Skoczyk <send@qrxnz.dev>
2025-09-14 09:44:19 +0200

    refactor: Migrate from just to go-task
    This commit migrates the project's command runner from 'just' to 'go-task'. The 'justfile' has been replaced with a 'Taskfile.yml', and the Nix flake has been updated to include 'go-task' instead of 'just'. The available commands ('serve', 'run') remain the same.

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
             alejandra
 
             # Others
-            just
+            go-task
             watchexec
           ];
         };
--- justfile
+++
@@ -1,4 +0,0 @@
-serve:
-    hugo server
-
-run: serve
