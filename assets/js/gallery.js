70408a01c87e26b833c7b3f76132d0d5939ced95
Karol Skoczyk <send@qrxnz.dev>
2026-05-31 15:14:02 +0200

    chore: remove unused site assets

--- assets/js/gallery.js
+++
@@ -1,9 +0,0 @@
-import PhotoSwipeLightbox from 'https://cdn.jsdelivr.net/npm/photoswipe@5.4.4/dist/photoswipe-lightbox.esm.js';
-
-const lightbox = new PhotoSwipeLightbox({
-  gallery: 'body',
-  children: 'a.gallery-image-link',
-  pswpModule: () => import('https://cdn.jsdelivr.net/npm/photoswipe@5.4.4/dist/photoswipe.esm.js')
-});
-
-lightbox.init();
